0dc4609bdbe4676d003e10005a52e6a26788c0c1
Tehran Bayramov <nicosspyros@gmail.com>
2024-11-16 00:15:44 +0400

    sdfdsdg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from '@/Components/Header/Header'
+import { navLinks } from '@/Components/navLinks'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header navLinks={navLinks} />
+        {children}</body>
     </html>
   )
 }
